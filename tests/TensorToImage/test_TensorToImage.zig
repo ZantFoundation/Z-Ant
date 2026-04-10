3d91949d2c0473ff4f56941576d04c5637eca705
Rocco Panizzi <rocco.panizzi@gmail.com>
2026-04-10 12:44:50 +0200

    implemented gadf test

---
+++ tests/TensorToImage/test_TensorToImage.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+test {
+    _ = @import("test_gasf.zig");
+    _ = @import("gadf/test_gadf.zig");
+    _ = @import("test_mtf.zig");
+}
--- tests/test_lib.zig
+++ tests/test_lib.zig
@@ -7,5 +7,5 @@ comptime {
     _ = @import("IR_graph/IR_graph.zig");
     _ = @import("TensorToImage/test_mtf.zig");
     _ = @import("TensorToImage/test_gasf.zig");
-    _ = @import("TensorToImage/test_gadf.zig");
+    _ = @import("TensorToImage/test_TensorToImage.zig");
 }
