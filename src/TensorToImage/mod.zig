3a92b3264e4eca0766218b5508af6a5bad4b4c38
Rocco Panizzi <rocco.panizzi@gmail.com>
2026-04-10 17:59:12 +0200

    added compound module to create RGB(GASF+GADF+MTF) in f32 format for CNNs

--- src/TensorToImage/mod.zig
+++ src/TensorToImage/mod.zig
@@ -4,3 +4,4 @@ pub const gadf = @import("gadf.zig");
 pub const gaf_utils = @import("gaf_utils.zig");
 pub const colormap = @import("colormap.zig");
 pub const matrixToBmp = @import("matrixToBmp.zig");
+pub const compound = @import("compound.zig");
--- tests/test_lib.zig
+++ tests/test_lib.zig
@@ -4,7 +4,5 @@ comptime {
     _ = @import("Core/test_core.zig");
     _ = @import("Utils/test_utils.zig");
     _ = @import("IR_graph/IR_graph.zig");
-    _ = @import("TensorToImage/test_mtf.zig");
-    _ = @import("TensorToImage/test_gasf.zig");
-    _ = @import("TensorToImage/gadf/test_gadf.zig");
+    _ = @import("TensorToImage/test_TensorToImage.zig");
 }
